1834b523c46045370d85de3004262235a96696a7
Naoya Ito <i.naoya@gmail.com>
2013-03-15 20:39:10 +0900

    fix issue #6

--- lib/Text/Md2Inao.pm
+++ lib/Text/Md2Inao.pm
@@ -188,6 +188,10 @@ sub prepare_text_for_markdown {
 
     ## Work Around: 先頭空白は字下げとみなし全角空白に置き換える (issue #4)
     $text =~ s/^[ ]{1,3}([^ ])/　$1/mg;
+
+    ## Work Around: リストの後にコードブロックが続くとだめな問題 (issue #6)
+    $text =~ s!- (.*?)\n\n    !- $1\n\n　\n\n    !g;
+
     return $text;
 }
 
