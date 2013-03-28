94b2c7f0af3f6329e20af3ef1e5aa8c91ea7c5e7
Naoya Ito <i.naoya@gmail.com>
2013-03-28 21:01:10 +0900

    add some tags to InDesign

--- lib/Text/Md2Inao/Builder/InDesign.pm
+++ lib/Text/Md2Inao/Builder/InDesign.pm
@@ -4,6 +4,9 @@ use utf8;
 use strict;
 use warnings;
 
+use Text::Md2Inao::Logger;
+use Text::Md2Inao::Util;
+
 use Text::Md2Inao::Builder;
 use parent qw/Text::Md2Inao::Builder/;
 
@@ -14,6 +17,19 @@ case default => sub {
 
 case text => sub {
     my ($c, $text) = @_;
+    if ($text =~ m!\(注:! or $c->in_footnote) {
+        $text = replace_note_parenthesis($c, $text, '注');
+        $text =~ s!◆注/◆!<fnStart:><pstyle:注釈>!g;
+        $text =~ s!◆/注◆!<fnEnd:><cstyle:>!g;
+    }
+    # 改行を取り除く
+    $text =~ s/(\n|\r)//g;
+    # キャプション
+    if ($text =~ s!^●(.+?)::(.+)!●$1\t$2!) {
+        $text =~ s!\[(.+)\]$!\n$1!;
+    }
+    # リストスタイル文字の変換
+    $text = to_list_style($text);
     return $text;
 };
 
@@ -22,9 +38,33 @@ case "h1" => sub {
     return sprintf "<ParaStyle:大見出し>%s\n", $h->as_trimmed_text;
 };
 
+case "h2" => sub {
+    my ($c, $h) = @_;
+    return sprintf "<ParaStyle:中見出し>%s\n", $h->as_trimmed_text;
+};
+
+case "h3" => sub {
+    my ($c, $h) = @_;
+    return sprintf "<ParaStyle:小見出し>%s\n", $h->as_trimmed_text;
+};
+
 case "strong" => sub {
     my ($c, $h) = @_;
     return sprintf "<CharStyle:太字>%s<CharStyle:>", $h->as_trimmed_text;
+};
+
+case "em" => sub {
+    my ($c, $h) = @_;
+    my $ret;
+    $ret .= $c->use_special_italic ? '<CharStyle:イタリック（変形斜体）>' : '<CharStyle:イタリック（変形斜体）>';
+    $ret .= $h->as_trimmed_text;
+    $ret .= '<CharStyle:>';
+    return $ret;
+};
+
+case "code" => sub {
+    my ($c, $h) = @_;
+    return sprintf "<CharStyle:コマンド>%s<CharStyle:>", $h->as_trimmed_text;
 };
 
 case p => sub {
@@ -35,5 +75,44 @@ case p => sub {
     }
 };
 
-1;
+## FIXME: https://github.com/inao/idtagreplacer/blob/master/sample/tagconf.xml の自由置換設定を反映させる必要
+case kbd => sub {
+    my ($c, $h) = @_;
+    sprintf "<cFont:Key Mother>%s<cFont:>" ,$h->as_trimmed_text;
+};
 
+case span => sub {
+    my ($c, $h) = @_;
+    if ($h->attr('class') eq 'red') {
+        return sprintf "<CharStyle:赤字>%s<CharStyle:>", $h->as_trimmed_text;
+    }
+    elsif ($h->attr('class') eq 'ruby') {
+        my $ruby = $h->as_trimmed_text;
+        $ruby =~ s!(.+)\((.+)\)!<cr:1><crstr:$2><cmojir:0>$1<cr:><crstr:><cmojir:>!;
+        return $ruby;
+    }
+    elsif ($h->attr('class') eq 'symbol') {
+        return sprintf "◆%s◆",$h->as_trimmed_text;
+    }
+    else {
+        return fallback_to_html($h);
+    }
+};
+
+case blockquote => sub {
+    my ($c, $h) = @_;
+    $c->in_quote_block(1);
+    my $blockquote = '';
+    for ($h->content_list) {
+        $blockquote .= $c->parse_element($_);
+    }
+    $blockquote =~ s/(\s)//g;
+    $c->in_quote_block(0);
+
+    return <<EOF;
+<ParaStyle:引用>
+<ParaStyle:引用>$blockquote
+EOF
+};
+
+1;
---
+++ t/30_indesign_basic_syntax.t
@@ -0,0 +1,99 @@
+use utf8;
+
+use Test::Base;
+use Text::Md2Inao;
+use Text::Md2Inao::Builder::InDesign;
+
+use Encode;
+
+plan tests => 1 * blocks;
+
+run_is in => 'expected';
+
+sub md2inao {
+    my $p = Text::Md2Inao->new({
+        default_list           => 'disc',
+        max_list_length        => 63,
+        max_inline_list_length => 55,
+        builder                => Text::Md2Inao::Builder::InDesign->new,
+    });
+    $p->parse($_);
+}
+
+__END__
+=== h1
+--- in md2inao
+# 見出し1（大見出し、節）
+--- expected
+<ParaStyle:大見出し>見出し1（大見出し、節）
+
+=== h2
+--- in md2inao
+## 見出し2（中見出し、項）
+--- expected
+<ParaStyle:中見出し>見出し2（中見出し、項）
+
+=== h3
+--- in md2inao
+### 見出し3（小見出し、目）
+--- expected
+<ParaStyle:小見出し>見出し3（小見出し、目）
+
+=== p
+--- in md2inao
+　段落冒頭の字下げは、このように手動でお願いします。
+改行は、（改行）このように自動で取り除かれます。
+--- expected
+<ParaStyle:本文>　段落冒頭の字下げは、このように手動でお願いします。改行は、（改行）このように自動で取り除かれます。
+
+=== strong
+--- in md2inao
+これは**太字**です
+--- expected
+<ParaStyle:本文>これは<CharStyle:太字>太字<CharStyle:>です
+
+=== em
+--- in md2inao
+これは_イタリック_です
+--- expected
+<ParaStyle:本文>これは<CharStyle:イタリック（変形斜体）>イタリック<CharStyle:>です
+
+=== code
+--- in md2inao
+これは`インラインのコード`です
+--- expected
+<ParaStyle:本文>これは<CharStyle:コマンド>インラインのコード<CharStyle:>です
+
+=== footnote
+--- in md2inao
+これは(注:ここは注釈です)です
+--- expected
+<ParaStyle:本文>これは<fnStart:><pstyle:注釈>ここは注釈です<fnEnd:><cstyle:>です
+
+=== kbd
+--- SKIP in md2inao
+通常の本文<kbd>Enter</kbd>（←キーボードフォント）
+--- expected
+<ParaStyle:本文>通常の本文<cFont:Key Mother><00A9><cFont:>（←キーボードフォント）
+
+=== red
+--- in md2inao
+これは<span class="red">赤文字</span>です
+--- expected
+<ParaStyle:本文>これは<CharStyle:赤字>赤文字<CharStyle:>です
+
+=== ruby
+--- in md2inao
+ルビつきの語<span class="ruby">外村(ほかむら)</span>です
+--- expected
+<ParaStyle:本文>ルビつきの語<cr:1><crstr:ほかむら><cmojir:0>外村<cr:><crstr:><cmojir:>です
+
+=== blockquote
+--- in md2inao
+> これは引用です。
+> これは引用です。
+> これは引用です。
+> これは**強調**と_イタリック_です。
+--- expected
+<ParaStyle:引用>
+<ParaStyle:引用>これは引用です。これは引用です。これは引用です。これは<CharStyle:太字>強調<CharStyle:>と<CharStyle:イタリック（変形斜体）>イタリック<CharStyle:>です。
