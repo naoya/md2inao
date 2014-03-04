d462fee7df1c849a34d066fe14d86bd8c3384ee7
Syohei YOSHIDA <syohex@gmail.com>
2014-03-04 15:51:01 +0900

    Fix for old Perl
    `//`(Defined-or operator) was introduced at Perl 5.10.

--- bin/md2inao.pl
+++ bin/md2inao.pl
@@ -27,7 +27,7 @@ my $p = Text::Md2Inao->new({
     max_inline_list_length => 55,
 });
 
-print encode($output_encoding // 'utf-8', $p->parse($text));
+print encode(defined($output_encoding) ? $output_encoding : 'utf-8', $p->parse($text));
 
 __END__
 
